399e0f5888824eedff73f697da54525f6f57159b
James Pound <jamespoundiv@gmail.com>
2026-02-25 20:11:57 -0500

    Updated with Android build files and app icon for iOS web app

---
+++ android/app/src/main/java/com/nethack3d/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.nethack3d.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.nethack3d.app",
+  appName: "NetHack 3D",
+  webDir: "dist",
+  bundledWebRuntime: false,
+};
+
+export default config;
